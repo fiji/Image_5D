e438120cd9e4d78e929975536719fd404c789ebf
Curtis Rueden <ctrueden@wisc.edu>
2011-11-04 13:11:03 -0500

    Update version to 1.2.6-SNAPSHOT.

--- src/main/java/i5d/Image5D.java
+++ src/main/java/i5d/Image5D.java
@@ -90,7 +90,7 @@ public class Image5D extends ImagePlus {
     
     // All references to data of image size and larger should be set to null in Image5D.flush().
     
-    public static final String VERSION = "1.2.5";
+    public static final String VERSION = "1.2.6-DEV";
     
 	static final int nDefaultDimensions = 5;
 	protected int nDimensions = nDefaultDimensions;
